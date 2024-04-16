e508e24811357612709a22a788ef01c5ad81b4a3
CHia <dozeuto@gmail.com>
2024-04-16 14:50:46 +0800

    update binary_search_tree

--- tree/binary_search_tree.go
+++ tree/binary_search_tree.go
@@ -1,35 +1,101 @@
 package tree
 
-import "letsgo/pkg"
+import (
+	. "letsgo/pkg"
+)
 
 type binarySearchTree struct {
-	root *pkg.TreeNode
+	root *TreeNode
 }
 
 func newBinarySearchTree() *binarySearchTree {
-	return &binarySearchTree{}
+	bst := &binarySearchTree{}
+	bst.root = nil
+	return bst
 }
 
-func (bst *binarySearchTree) getRoot() *pkg.TreeNode {
+func (bst *binarySearchTree) getRoot() *TreeNode {
 	return bst.root
 }
 
-func (bst *binarySearchTree) insert(val int) {
-	bst.root = bst.insertRec(bst.root, val)
+func (bst *binarySearchTree) search(num int) *TreeNode {
+	node := bst.root
+	for node != nil {
+		if num < node.Val.(int) {
+			node = node.Left
+		} else if num > node.Val.(int) {
+			node = node.Right
+		} else {
+			return node
+		}
+	}
+	return nil
 }
 
-func (bst *binarySearchTree) insertRec(node *pkg.TreeNode, val int) *pkg.TreeNode {
+func (bst *binarySearchTree) Insert(value int) {
+	bst.root = bst.insert(bst.root, value)
+}
+
+func (bst *binarySearchTree) insert(node *TreeNode, value int) *TreeNode {
 	if node == nil {
-		return &pkg.TreeNode{Val: val}
+		return NewTreeNode(value)
 	}
-	if val < node.Val.(int) {
-		node.Left = bst.insertRec(node.Left, val)
+
+	if value < node.Val.(int) {
+		node.Left = bst.insert(node.Left, value)
 	} else {
-		node.Right = bst.insertRec(node.Right, val)
+		node.Right = bst.insert(node.Right, value)
 	}
 	return node
 }
 
+func (bst *binarySearchTree) Remove(value int) {
+	bst.root = bst.removeNode(bst.root, value)
+}
+
+func (bst *binarySearchTree) removeNode(node *TreeNode, value int) *TreeNode {
+	if node == nil {
+		return nil
+	}
+
+	if value < node.Val.(int) {
+		node.Left = bst.removeNode(node.Left, value)
+	} else if value > node.Val.(int) {
+		node.Right = bst.removeNode(node.Right, value)
+	} else {
+		if node.Left == nil {
+			return node.Right
+		} else if node.Right == nil {
+			return node.Left
+		}
+
+		minNode := bst.findMin(node.Right)
+		node.Val = minNode.Val
+		node.Right = bst.removeNode(node.Right, minNode.Val.(int))
+	}
+	return node
+}
+
+func (bst *binarySearchTree) findMin(node *TreeNode) *TreeNode {
+	for node.Left != nil {
+		node = node.Left
+	}
+	return node
+}
+
+func (bst *binarySearchTree) inOrder() (res []int) {
+	bst.inOrderHelper(bst.root, &res)
+	return
+}
+
+func (bst *binarySearchTree) inOrderHelper(node *TreeNode, res *[]int) {
+	if node != nil {
+		bst.inOrderHelper(node.Left, res)
+		*res = append(*res, node.Val.(int))
+		bst.inOrderHelper(node.Right, res)
+	}
+}
+
 func (bst *binarySearchTree) print() {
-	pkg.PrintTree(bst.root)
+	PrintTree(bst.root)
 }
--- tree/binary_search_tree_test.go
+++ tree/binary_search_tree_test.go
@@ -1,7 +1,23 @@
 package tree
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestBinarySearchTree(t *testing.T) {
+	bst := newBinarySearchTree()
+	nums := []int{8, 4, 12, 2, 6, 10, 14, 1, 3, 5, 7, 9, 11, 13, 15}
+	for _, num := range nums {
+		bst.Insert(num)
+	}
 
+	bst.print()
+
+	// bst.Remove(1)
+	// bst.print()
+	// bst.Remove(2)
+	// bst.print()
+	res := bst.inOrder()
+	fmt.Println(res)
 }
